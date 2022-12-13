6a53f769b6bb150746f6fba0a77c952c6d99c704
Karol Przybylski <karol.przybylski@esm-technology.pl>
2022-12-13 13:27:22 +0100

    +change boot partitions

---
+++ change_boot_partition.sh
@@ -0,0 +1,16 @@
+
+boot_part=$(hexdump -e '1/1 "%01x|"' -n 1 -s 168 -C /dev/mtd3|cut -f 1 -d "|"|head -n1)
+
+dd if=/dev/mtd2 of=boot.bin bs=336 count=1
+
+if [ $1 -eq 1 ]
+  printf '\x01' | dd of=boot.bin bs=1 seek=168 count=1 conv=notrunc
+else
+  printf '\x00' | dd of=boot.bin bs=1 seek=168 count=1 conv=notrunc
+
+fi
+
+dd if=boot.bin of=/dev/mtd2
+dd if=boot.bin of=/dev/mtd3
+
+
